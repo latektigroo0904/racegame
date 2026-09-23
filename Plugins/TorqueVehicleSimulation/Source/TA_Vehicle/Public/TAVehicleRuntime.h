3666154851a2c36135d83e4d44ade174298ff90e
latektigroo0904 <292cgngngj@privaterelay.appleid.com>
2026-09-23 08:17:59 +0200

    feat: add native compiled vehicle configuration

---
+++ Plugins/TorqueVehicleSimulation/Source/TA_Vehicle/Public/TAVehicleRuntime.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TACoreTypes.h"
+
+struct TA_VEHICLE_API FTAVehicleCompiledConfig
+{
+    FTASimulationVersion Version;
+
+    FName DefinitionId = NAME_None;
+
+    double MassKg = 0.0;
+    FVector3d CenterOfMassMeters = FVector3d::ZeroVector;
+    FVector3d PrincipalInertiaKgm2 = FVector3d::ZeroVector;
+
+    double LengthMeters = 0.0;
+    double WidthMeters = 0.0;
+    double HeightMeters = 0.0;
+    double WheelbaseMeters = 0.0;
+    double TrackFrontMeters = 0.0;
+    double TrackRearMeters = 0.0;
+
+    int32 WheelCount = 4;
+
+    uint32 PhysicsConfigHash = 0;
+};
