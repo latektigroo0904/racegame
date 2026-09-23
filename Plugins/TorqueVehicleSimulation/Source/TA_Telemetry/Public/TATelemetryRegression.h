4800114b405c04e8b19b031e8b39b6613c2501ee
latektigroo0904 <292cgngngj@privaterelay.appleid.com>
2026-09-23 11:19:32 +0200

    feat: add rich telemetry scenario profile report schema

--- Plugins/TorqueVehicleSimulation/Source/TA_Telemetry/Public/TATelemetryRegression.h
+++ Plugins/TorqueVehicleSimulation/Source/TA_Telemetry/Public/TATelemetryRegression.h
@@ -2,6 +2,7 @@
 
 #include "CoreMinimal.h"
 #include "TATelemetryBuffer.h"
+#include "TARegressionEnvelope.h"
 
 enum class ETATelemetryMetric : uint8
 {
@@ -107,6 +108,77 @@ struct TA_TELEMETRY_API FTATelemetryRegressionResult
     TArray<FString> Failures;
 };
 
+struct TA_TELEMETRY_API FTATelemetryMetricProfile
+{
+    ETATelemetryMetric Metric =
+        ETATelemetryMetric::ChassisSpeedMps;
+
+    int32 WheelIndex = INDEX_NONE;
+
+    double StartFraction01 = 0.0;
+    double EndFraction01 = 1.0;
+
+    // Trailing fraction of the selected window.
+    double SteadyStateFraction01 = 0.25;
+
+    FTARegressionRange MinimumEnvelope;
+    FTARegressionRange MaximumEnvelope;
+    FTARegressionRange SteadyStateEnvelope;
+};
+
+struct TA_TELEMETRY_API FTATelemetryScenarioProfileConfig
+{
+    FName ScenarioId = NAME_None;
+
+    int32 BaselineVersion = 1;
+
+    bool bTrustedBaseline = false;
+
+    uint32 ExpectedPhysicsConfigHash = 0;
+
+    int32 MinimumRequiredSamples = 1;
+
+    TArray<FTATelemetryMetricProfile> Metrics;
+};
+
+struct TA_TELEMETRY_API FTATelemetryMetricProfileResult
+{
+    ETATelemetryMetric Metric =
+        ETATelemetryMetric::ChassisSpeedMps;
+
+    int32 WheelIndex = INDEX_NONE;
+
+    double StartFraction01 = 0.0;
+    double EndFraction01 = 1.0;
+
+    FTARegressionRange MinimumEnvelope;
+    FTARegressionRange MaximumEnvelope;
+    FTARegressionRange SteadyStateEnvelope;
+
+    FTARegressionMetricResult Evaluation;
+};
+
+struct TA_TELEMETRY_API FTATelemetryScenarioProfileResult
+{
+    FName ScenarioId = NAME_None;
+
+    int32 BaselineVersion = 0;
+
+    bool bTrustedBaseline = false;
+
+    uint32 ExpectedPhysicsConfigHash = 0;
+    uint32 ObservedPhysicsConfigHash = 0;
+
+    bool bPhysicsConfigHashConsistent = true;
+    bool bPhysicsConfigHashMatched = true;
+    bool bPassed = false;
+
+    int32 SampleCount = 0;
+
+    TArray<FTATelemetryMetricProfileResult> MetricResults;
+    TArray<FString> Failures;
+};
+
 namespace TATelemetryRegression
 {
     TA_TELEMETRY_API bool ValidateConfig(
@@ -123,4 +195,18 @@ namespace TATelemetryRegression
 
     TA_TELEMETRY_API FString ExportJsonLines(
         const FTATelemetryRegressionResult& Result);
+
+    TA_TELEMETRY_API bool ValidateProfileConfig(
+        const FTATelemetryScenarioProfileConfig& Config);
+
+    TA_TELEMETRY_API bool EvaluateProfile(
+        const FTATelemetryRingBuffer& Buffer,
+        const FTATelemetryScenarioProfileConfig& Config,
+        FTATelemetryScenarioProfileResult& OutResult);
+
+    TA_TELEMETRY_API FString ExportProfileCsv(
+        const FTATelemetryScenarioProfileResult& Result);
+
+    TA_TELEMETRY_API FString ExportProfileJsonLines(
+        const FTATelemetryScenarioProfileResult& Result);
 }
