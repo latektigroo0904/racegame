d9bd2da4711aa422155f77c41bcb69967250cd06
latektigroo0904 <292cgngngj@privaterelay.appleid.com>
2026-09-23 10:38:17 +0200

    test: validate compiled physics config hash geometry and COM transform

--- Plugins/TorqueVehicleSimulation/Source/TA_Vehicle/Private/Tests/TAVehicleDefinitionTests.cpp
+++ Plugins/TorqueVehicleSimulation/Source/TA_Vehicle/Private/Tests/TAVehicleDefinitionTests.cpp
@@ -72,4 +72,113 @@ bool FTAVehicleDefinitionCompileTest::RunTest(const FString& Parameters)
     return true;
 }
 
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(
+    FTAVehicleDefinitionHashSensitivityTest,
+    "TorqueAtlas.Vehicle.Definition.HashChangesWithPhysics",
+    EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
+
+bool FTAVehicleDefinitionHashSensitivityTest::RunTest(const FString& Parameters)
+{
+    UTAVehicleDefinition* Definition =
+        NewObject<UTAVehicleDefinition>();
+
+    FTAVehicleCompiledConfig Baseline;
+    FTAValidationResult BaselineValidation;
+
+    TestTrue(
+        TEXT("Baseline definition compiles"),
+        Definition->BuildCompiledConfig(
+            Baseline,
+            BaselineValidation));
+
+    Definition->Tire.RadialStiffnessNPerM +=
+        1000.0;
+
+    FTAVehicleCompiledConfig Modified;
+    FTAValidationResult ModifiedValidation;
+
+    TestTrue(
+        TEXT("Modified definition compiles"),
+        Definition->BuildCompiledConfig(
+            Modified,
+            ModifiedValidation));
+
+    TestTrue(
+        TEXT("Handling-critical tire change changes physics config hash"),
+        Modified.PhysicsConfigHash
+            != Baseline.PhysicsConfigHash);
+
+    return true;
+}
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(
+    FTAVehicleDefinitionGeometryValidationTest,
+    "TorqueAtlas.Vehicle.Definition.RejectsDegenerateSuspension",
+    EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
+
+bool FTAVehicleDefinitionGeometryValidationTest::RunTest(const FString& Parameters)
+{
+    UTAVehicleDefinition* Definition =
+        NewObject<UTAVehicleDefinition>();
+
+    Definition->FrontSuspension.UpperInnerB =
+        Definition->FrontSuspension.UpperInnerA;
+
+    FTAVehicleCompiledConfig Config;
+    FTAValidationResult Validation;
+
+    TestFalse(
+        TEXT("Degenerate front suspension fails asset compilation"),
+        Definition->BuildCompiledConfig(
+            Config,
+            Validation));
+
+    TestTrue(
+        TEXT("Degenerate front suspension emits validation errors"),
+        Validation.HasErrors());
+
+    return true;
+}
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(
+    FTAVehicleDefinitionComTransformTest,
+    "TorqueAtlas.Vehicle.Definition.CompilesHardpointsToComLocal",
+    EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
+
+bool FTAVehicleDefinitionComTransformTest::RunTest(const FString& Parameters)
+{
+    UTAVehicleDefinition* Definition =
+        NewObject<UTAVehicleDefinition>();
+
+    const FVector AuthoredWheelCenter =
+        Definition->FrontSuspension.WheelCenter;
+
+    Definition->Mass.CenterOfMassMeters =
+        FVector(0.10, 0.0, 0.05);
+
+    FTAVehicleCompiledConfig Config;
+    FTAValidationResult Validation;
+
+    TestTrue(
+        TEXT("Definition with non-zero COM compiles"),
+        Definition->BuildCompiledConfig(
+            Config,
+            Validation));
+
+    const FVector3d CompiledWheelCenter =
+        Config.FourWheelRuntime
+            .FrontAxle.RightGeometry
+            .Hardpoints.WheelCenterReference;
+
+    TestTrue(
+        TEXT("Compiled hardpoint is translated into COM-local frame"),
+        CompiledWheelCenter.Equals(
+            FVector3d(AuthoredWheelCenter)
+                - FVector3d(Definition->Mass.CenterOfMassMeters),
+            1.0e-9));
+
+    return true;
+}
+
 #endif
