1aa582ec2b0bbd191d1c0d6048b5c2a5dcb70ecd
latektigroo0904 <292cgngngj@privaterelay.appleid.com>
2026-09-23 10:26:46 +0200

    feat: implement compliant tire rear axle runtime

--- Plugins/TorqueVehicleSimulation/Source/TA_Vehicle/Private/TARearAxleRuntime.cpp
+++ Plugins/TorqueVehicleSimulation/Source/TA_Vehicle/Private/TARearAxleRuntime.cpp
@@ -87,3 +87,94 @@ bool TARearAxleRuntime::Resolve(
     OutOutput.bSolved = true;
     return true;
 }
+
+
+bool TARearAxleRuntime::ResolveWithTireCompliance(
+    const FTAChassisState& Chassis,
+    const FTARearAxleRuntimeConfig& Config,
+    const FTARearAxleSolveInput& Input,
+    const FTATireRuntimeConfig& LeftTireConfig,
+    const FTATireRuntimeConfig& RightTireConfig,
+    const double DeltaTimeSeconds,
+    FTARearAxleRuntimeState& InOutState,
+    FTATireRuntimeState& InOutLeftTireState,
+    FTATireRuntimeState& InOutRightTireState,
+    FTARearAxleSolveOutput& OutOutput)
+{
+    OutOutput = FTARearAxleSolveOutput{};
+
+    if (DeltaTimeSeconds <= 0.0 ||
+        !TAMultiLinkSolver::ValidateConfig(Config.RightGeometry))
+    {
+        return false;
+    }
+
+    const FTAMultiLinkSolverConfig LeftGeometry =
+        TAMultiLinkSolver::MirrorAcrossCenterline(
+            Config.RightGeometry);
+
+    if (!TAMultiLinkSolver::ValidateConfig(LeftGeometry))
+    {
+        return false;
+    }
+
+    if (!TAMultiLinkContactResolver::ResolveCompliantRoadContact(
+            Chassis,
+            LeftGeometry,
+            Config.LeftSuspension,
+            LeftTireConfig,
+            Input.LeftDamage,
+            Input.LeftRoad,
+            DeltaTimeSeconds,
+            InOutState.LeftGeometry,
+            InOutState.LeftSuspension,
+            InOutLeftTireState,
+            OutOutput.LeftContact))
+    {
+        return false;
+    }
+
+    if (!TAMultiLinkContactResolver::ResolveCompliantRoadContact(
+            Chassis,
+            Config.RightGeometry,
+            Config.RightSuspension,
+            RightTireConfig,
+            Input.RightDamage,
+            Input.RightRoad,
+            DeltaTimeSeconds,
+            InOutState.RightGeometry,
+            InOutState.RightSuspension,
+            InOutRightTireState,
+            OutOutput.RightContact))
+    {
+        return false;
+    }
+
+    TAMultiLinkContactResolver::ApplyAntiRollBarToPair(
+        Config.AntiRollBar,
+        OutOutput.LeftContact,
+        OutOutput.RightContact);
+
+    OutOutput.LeftVehicleContact =
+        TAMultiLinkContactResolver::BuildVehicleWheelContactInput(
+            OutOutput.LeftContact);
+
+    OutOutput.RightVehicleContact =
+        TAMultiLinkContactResolver::BuildVehicleWheelContactInput(
+            OutOutput.RightContact);
+
+    OutOutput.LeftToeRad =
+        OutOutput.LeftContact.Geometry.ToeRad;
+
+    OutOutput.RightToeRad =
+        OutOutput.RightContact.Geometry.ToeRad;
+
+    OutOutput.LeftCamberRad =
+        OutOutput.LeftContact.Geometry.CamberRad;
+
+    OutOutput.RightCamberRad =
+        OutOutput.RightContact.Geometry.CamberRad;
+
+    OutOutput.bSolved = true;
+    return true;
+}
