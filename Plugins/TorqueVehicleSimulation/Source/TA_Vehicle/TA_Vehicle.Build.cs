ccad2bdc187621d4e792785ff7aec97c9649003d
latektigroo0904 <292cgngngj@privaterelay.appleid.com>
2026-09-23 08:17:29 +0200

    build: add TA_Vehicle module rules

---
+++ Plugins/TorqueVehicleSimulation/Source/TA_Vehicle/TA_Vehicle.Build.cs
@@ -0,0 +1,19 @@
+using UnrealBuildTool;
+
+public class TA_Vehicle : ModuleRules
+{
+    public TA_Vehicle(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+                "CoreUObject",
+                "Engine",
+                "TA_Core"
+            }
+        );
+    }
+}
