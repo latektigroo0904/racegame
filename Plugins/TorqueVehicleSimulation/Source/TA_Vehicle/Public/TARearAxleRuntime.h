4ff9404fa24106c0cbc3a9a65583b100f09a5b45
latektigroo0904 <292cgngngj@privaterelay.appleid.com>
2026-09-23 10:12:29 +0200

    feat: add rear multi link axle runtime API

---
+++ Plugins/TorqueVehicleSimulation/Source/TA_Vehicle/Public/TARearAxleRuntime.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TAMultiLinkContactResolver.h"
+
+struct TA_VEHICLE_API FTARearAxleRuntimeConfig
+{
+    FTAMultiLinkSolverConfig RightGeometry;
+
+    FTASuspensionRuntimeConfig LeftSuspension;
+    FTASuspensionRuntimeConfig RightSuspension;
+
+    FTAAntiRollBarConfig AntiRollBar;
+};
+
+struct TA_VEHICLE_API FTARearAxleRuntimeState
+{
+    FTAMultiLinkRuntimeState LeftGeometry;
+    FTAMultiLinkRuntimeState RightGeometry;
+
+    FTASuspensionRuntimeState LeftSuspension;
+    FTASuspensionRuntimeState RightSuspension;
+};
+
+struct TA_VEHICLE_API FTARearAxleSolveInput
+{
+    FTARoadPlane LeftRoad;
+    FTARoadPlane RightRoad;
+
+    FTAMultiLinkDamageOffsets LeftDamage;
+    FTAMultiLinkDamageOffsets RightDamage;
+};
+
+struct TA_VEHICLE_API FTARearAxleSolveOutput
+{
+    bool bSolved = false;
+
+    FTAResolvedMultiLinkContact LeftContact;
+    FTAResolvedMultiLinkContact RightContact;
+
+    FTAWheelContactInput LeftVehicleContact;
+    FTAWheelContactInput RightVehicleContact;
+
+    double LeftToeRad = 0.0;
+    double RightToeRad = 0.0;
+
+    double LeftCamberRad = 0.0;
+    double RightCamberRad = 0.0;
+};
+
+namespace TARearAxleRuntime
+{
+    TA_VEHICLE_API bool Resolve(
+        const FTAChassisState& Chassis,
+        const FTARearAxleRuntimeConfig& Config,
+        const FTARearAxleSolveInput& Input,
+        double LeftWheelRadiusM,
+        double RightWheelRadiusM,
+        double DeltaTimeSeconds,
+        FTARearAxleRuntimeState& InOutState,
+        FTARearAxleSolveOutput& OutOutput);
+}
