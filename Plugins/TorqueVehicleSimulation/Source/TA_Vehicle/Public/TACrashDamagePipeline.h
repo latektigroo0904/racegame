805905b511517673772594cd56660472b4342674
latektigroo0904 <292cgngngj@privaterelay.appleid.com>
2026-09-23 10:56:55 +0200

    feat: add deterministic crash damage processing pipeline API

---
+++ Plugins/TorqueVehicleSimulation/Source/TA_Vehicle/Public/TACrashDamagePipeline.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TACollisionStructureCoupling.h"
+#include "TAStructureDamageBridge.h"
+#include "TAVehicleDamageRouter.h"
+
+struct TA_VEHICLE_API FTACrashDamagePipelineConfig
+{
+    FTACollisionStructureCouplingConfig CollisionCoupling;
+    FTAStructureSolverConfig StructureSolver;
+    FTAStructureDamageBridgeConfig DamageBridge;
+    FTAVehicleDamageRoutingConfig DamageRouting;
+};
+
+struct TA_VEHICLE_API FTACrashDamagePipelineInput
+{
+    uint64 SimulationTick = 0;
+    uint16 Substep = 0;
+
+    double StructureDeltaTimeSeconds = 1.0 / 240.0;
+
+    FTACollisionStructureInput Collision;
+};
+
+struct TA_VEHICLE_API FTACrashDamagePipelineOutput
+{
+    bool bCollisionProcessed = false;
+    bool bImpactSignalEmitted = false;
+    bool bStructureSignalsEmitted = false;
+    bool bDamageRouted = false;
+
+    int32 EmittedSignalCount = 0;
+
+    FTACollisionStructureOutput Collision;
+    FTAVehicleDamageRoutingOutput Routing;
+};
+
+namespace TACrashDamagePipeline
+{
+    TA_VEHICLE_API bool ProcessImpact(
+        const FTACrashDamagePipelineConfig& PipelineConfig,
+        const FTAVehicleRuntimeConfig& VehicleConfig,
+        const FTACrashDamagePipelineInput& Input,
+        FTAChassisState& InOutChassisState,
+        FTAVehicleRuntimeState& InOutVehicleState,
+        TArray<FTAStructureNode>& InOutStructureNodes,
+        TArray<FTADistanceConstraint>& InOutConstraints,
+        FTAStructureImpactScratch& InOutImpactScratch,
+        FTAStructureDamageBridgeState& InOutDamageBridgeState,
+        FTADamageEventQueue& InOutDamageQueue,
+        FTACrashDamagePipelineOutput& OutOutput);
+}
