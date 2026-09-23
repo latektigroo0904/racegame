122796b813ce3efa0966c57c2274efdb29c85b97
latektigroo0904 <292cgngngj@privaterelay.appleid.com>
2026-09-23 09:46:30 +0200

    feat: add structural displacement to suspension pickup binding API

---
+++ Plugins/TorqueVehicleSimulation/Source/TA_Vehicle/Public/TASuspensionDamageBinding.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TADoubleWishboneSolver.h"
+#include "TAStructureSolver.h"
+
+struct TA_VEHICLE_API FTAStructureDisplacementBinding
+{
+    TArray<int32> NodeIndices;
+    TArray<double> Weights;
+};
+
+struct TA_VEHICLE_API FTADoubleWishboneStructuralBindings
+{
+    FTAStructureDisplacementBinding UpperInnerA;
+    FTAStructureDisplacementBinding UpperInnerB;
+
+    FTAStructureDisplacementBinding LowerInnerA;
+    FTAStructureDisplacementBinding LowerInnerB;
+
+    FTAStructureDisplacementBinding TieRodInner;
+    FTAStructureDisplacementBinding DamperChassis;
+};
+
+namespace TASuspensionDamageBinding
+{
+    TA_VEHICLE_API bool ValidateBinding(
+        const FTAStructureDisplacementBinding& Binding,
+        int32 StructureNodeCount);
+
+    TA_VEHICLE_API bool ResolveBindingDisplacement(
+        TConstArrayView<FTAStructureNode> StructureNodes,
+        const FTAStructureDisplacementBinding& Binding,
+        FVector3d& OutDisplacementM);
+
+    TA_VEHICLE_API bool ResolveDoubleWishboneDamageOffsets(
+        TConstArrayView<FTAStructureNode> StructureNodes,
+        const FTADoubleWishboneStructuralBindings& Bindings,
+        FTADoubleWishboneDamageOffsets& OutOffsets);
+}
