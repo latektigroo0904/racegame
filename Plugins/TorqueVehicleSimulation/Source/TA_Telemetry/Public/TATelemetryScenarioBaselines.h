421b99e596c22f55db8262d28fd8d1b736b3312c
latektigroo0904 <292cgngngj@privaterelay.appleid.com>
2026-09-23 11:18:15 +0200

    feat: add provisional telemetry scenario baseline schema

---
+++ Plugins/TorqueVehicleSimulation/Source/TA_Telemetry/Public/TATelemetryScenarioBaselines.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TATelemetryRegression.h"
+
+enum class ETARegressionScenarioKind : uint8
+{
+    StaticSettle,
+    Acceleration,
+    Braking,
+    ConstantSteer,
+    AsymmetricRoad,
+    SyntheticFrontCornerCrash
+};
+
+struct TA_TELEMETRY_API FTATelemetryScenarioBaseline
+{
+    ETARegressionScenarioKind Kind =
+        ETARegressionScenarioKind::StaticSettle;
+
+    FName ScenarioId = NAME_None;
+
+    int32 BaselineVersion = 1;
+
+    // False until ranges have been measured and accepted from a real UE run.
+    bool bTrustedBaseline = false;
+
+    FString Purpose;
+
+    FTATelemetryRegressionConfig Regression;
+};
+
+namespace TATelemetryScenarioBaselines
+{
+    TA_TELEMETRY_API bool BuildProvisional(
+        ETARegressionScenarioKind Kind,
+        uint32 ExpectedPhysicsConfigHash,
+        FTATelemetryScenarioBaseline& OutBaseline);
+
+    TA_TELEMETRY_API void BuildAllProvisional(
+        uint32 ExpectedPhysicsConfigHash,
+        TArray<FTATelemetryScenarioBaseline>& OutBaselines);
+
+    TA_TELEMETRY_API bool ValidateBaseline(
+        const FTATelemetryScenarioBaseline& Baseline);
+}
