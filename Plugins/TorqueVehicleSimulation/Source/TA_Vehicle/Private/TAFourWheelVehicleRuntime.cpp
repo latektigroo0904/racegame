911df81ab9f7ee0b8a7eca7c95e0214f34614166
latektigroo0904 <292cgngngj@privaterelay.appleid.com>
2026-09-23 10:14:48 +0200

    feat: implement four wheel self resolved fixed step runtime

---
+++ Plugins/TorqueVehicleSimulation/Source/TA_Vehicle/Private/TAFourWheelVehicleRuntime.cpp
@@ -0,0 +1,136 @@
+#include "TAFourWheelVehicleRuntime.h"
+
+namespace
+{
+    int32 ToIndex(const ETAPrototypeWheelIndex Wheel)
+    {
+        return static_cast<int32>(Wheel);
+    }
+}
+
+bool TAFourWheelVehicleRuntime::Initialize(
+    const FTAVehicleRuntimeConfig& VehicleConfig,
+    FTAFourWheelRuntimeState& OutState)
+{
+    OutState = FTAFourWheelRuntimeState{};
+
+    if (VehicleConfig.Wheels.Num() != 4 ||
+        VehicleConfig.Tires.Num() != 4)
+    {
+        return false;
+    }
+
+    return TAVehicleSimulation::Initialize(
+        VehicleConfig,
+        OutState.Vehicle);
+}
+
+bool TAFourWheelVehicleRuntime::Step(
+    const FTAVehicleRuntimeConfig& VehicleConfig,
+    const FTAFourWheelRuntimeConfig& RuntimeConfig,
+    const FTAFourWheelStepInput& Input,
+    const double DeltaTimeSeconds,
+    FTAFourWheelRuntimeState& InOutState,
+    FTAFourWheelStepOutput& OutOutput)
+{
+    OutOutput = FTAFourWheelStepOutput{};
+
+    if (DeltaTimeSeconds <= 0.0 ||
+        VehicleConfig.Wheels.Num() != 4 ||
+        VehicleConfig.Tires.Num() != 4 ||
+        InOutState.Vehicle.Wheels.Num() != 4)
+    {
+        return false;
+    }
+
+    FTAFrontAxleSolveInput FrontInput;
+    FrontInput.Steering01 =
+        Input.Controls.Steering01;
+
+    FrontInput.LeftRoad =
+        Input.FrontLeftRoad;
+
+    FrontInput.RightRoad =
+        Input.FrontRightRoad;
+
+    FrontInput.LeftDamage =
+        Input.FrontLeftDamage;
+
+    FrontInput.RightDamage =
+        Input.FrontRightDamage;
+
+    if (!TAFrontAxleRuntime::Resolve(
+            InOutState.Vehicle.Chassis,
+            RuntimeConfig.FrontAxle,
+            FrontInput,
+            VehicleConfig.Wheels[
+                ToIndex(ETAPrototypeWheelIndex::FrontLeft)].RadiusM,
+            VehicleConfig.Wheels[
+                ToIndex(ETAPrototypeWheelIndex::FrontRight)].RadiusM,
+            DeltaTimeSeconds,
+            InOutState.FrontAxle,
+            OutOutput.FrontAxle))
+    {
+        return false;
+    }
+
+    FTARearAxleSolveInput RearInput;
+
+    RearInput.LeftRoad =
+        Input.RearLeftRoad;
+
+    RearInput.RightRoad =
+        Input.RearRightRoad;
+
+    RearInput.LeftDamage =
+        Input.RearLeftDamage;
+
+    RearInput.RightDamage =
+        Input.RearRightDamage;
+
+    if (!TARearAxleRuntime::Resolve(
+            InOutState.Vehicle.Chassis,
+            RuntimeConfig.RearAxle,
+            RearInput,
+            VehicleConfig.Wheels[
+                ToIndex(ETAPrototypeWheelIndex::RearLeft)].RadiusM,
+            VehicleConfig.Wheels[
+                ToIndex(ETAPrototypeWheelIndex::RearRight)].RadiusM,
+            DeltaTimeSeconds,
+            InOutState.RearAxle,
+            OutOutput.RearAxle))
+    {
+        return false;
+    }
+
+    FTAVehicleStepInput VehicleInput;
+    VehicleInput.Controls =
+        Input.Controls;
+
+    VehicleInput.WheelContacts.SetNum(4);
+
+    VehicleInput.WheelContacts[
+        ToIndex(ETAPrototypeWheelIndex::FrontLeft)] =
+        OutOutput.FrontAxle.LeftVehicleContact;
+
+    VehicleInput.WheelContacts[
+        ToIndex(ETAPrototypeWheelIndex::FrontRight)] =
+        OutOutput.FrontAxle.RightVehicleContact;
+
+    VehicleInput.WheelContacts[
+        ToIndex(ETAPrototypeWheelIndex::RearLeft)] =
+        OutOutput.RearAxle.LeftVehicleContact;
+
+    VehicleInput.WheelContacts[
+        ToIndex(ETAPrototypeWheelIndex::RearRight)] =
+        OutOutput.RearAxle.RightVehicleContact;
+
+    OutOutput.bContactsSolved = true;
+
+    return TAVehicleSimulation::Step(
+        VehicleConfig,
+        VehicleInput,
+        DeltaTimeSeconds,
+        InOutState.Vehicle,
+        OutOutput.Vehicle);
+}
