e3a708083166321b152c4a86144d97baa28816f1
latektigroo0904 <292cgngngj@privaterelay.appleid.com>
2026-09-23 10:12:42 +0200

    feat: implement mirrored rear multi link axle runtime

---
+++ Plugins/TorqueVehicleSimulation/Source/TA_Vehicle/Private/TARearAxleRuntime.cpp
@@ -0,0 +1,89 @@
+#include "TARearAxleRuntime.h"
+
+bool TARearAxleRuntime::Resolve(
+    const FTAChassisState& Chassis,
+    const FTARearAxleRuntimeConfig& Config,
+    const FTARearAxleSolveInput& Input,
+    const double LeftWheelRadiusM,
+    const double RightWheelRadiusM,
+    const double DeltaTimeSeconds,
+    FTARearAxleRuntimeState& InOutState,
+    FTARearAxleSolveOutput& OutOutput)
+{
+    OutOutput = FTARearAxleSolveOutput{};
+
+    if (DeltaTimeSeconds <= 0.0 ||
+        LeftWheelRadiusM <= UE_DOUBLE_SMALL_NUMBER ||
+        RightWheelRadiusM <= UE_DOUBLE_SMALL_NUMBER ||
+        !TAMultiLinkSolver::ValidateConfig(Config.RightGeometry))
+    {
+        return false;
+    }
+
+    const FTAMultiLinkSolverConfig LeftGeometry =
+        TAMultiLinkSolver::MirrorAcrossCenterline(
+            Config.RightGeometry);
+
+    if (!TAMultiLinkSolver::ValidateConfig(LeftGeometry))
+    {
+        return false;
+    }
+
+    if (!TAMultiLinkContactResolver::ResolveRoadContact(
+            Chassis,
+            LeftGeometry,
+            Config.LeftSuspension,
+            LeftWheelRadiusM,
+            Input.LeftDamage,
+            Input.LeftRoad,
+            DeltaTimeSeconds,
+            InOutState.LeftGeometry,
+            InOutState.LeftSuspension,
+            OutOutput.LeftContact))
+    {
+        return false;
+    }
+
+    if (!TAMultiLinkContactResolver::ResolveRoadContact(
+            Chassis,
+            Config.RightGeometry,
+            Config.RightSuspension,
+            RightWheelRadiusM,
+            Input.RightDamage,
+            Input.RightRoad,
+            DeltaTimeSeconds,
+            InOutState.RightGeometry,
+            InOutState.RightSuspension,
+            OutOutput.RightContact))
+    {
+        return false;
+    }
+
+    TAMultiLinkContactResolver::ApplyAntiRollBarToPair(
+        Config.AntiRollBar,
+        OutOutput.LeftContact,
+        OutOutput.RightContact);
+
+    OutOutput.LeftVehicleContact =
+        TAMultiLinkContactResolver::BuildVehicleWheelContactInput(
+            OutOutput.LeftContact);
+
+    OutOutput.RightVehicleContact =
+        TAMultiLinkContactResolver::BuildVehicleWheelContactInput(
+            OutOutput.RightContact);
+
+    OutOutput.LeftToeRad =
+        OutOutput.LeftContact.Geometry.ToeRad;
+
+    OutOutput.RightToeRad =
+        OutOutput.RightContact.Geometry.ToeRad;
+
+    OutOutput.LeftCamberRad =
+        OutOutput.LeftContact.Geometry.CamberRad;
+
+    OutOutput.RightCamberRad =
+        OutOutput.RightContact.Geometry.CamberRad;
+
+    OutOutput.bSolved = true;
+    return true;
+}
