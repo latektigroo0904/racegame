f621fde0fccc3aa1867900113c13623fc4b35c1b
latektigroo0904 <292cgngngj@privaterelay.appleid.com>
2026-09-23 10:41:58 +0200

    fix: solve rear anti roll inside compliant tire equilibrium

--- Plugins/TorqueVehicleSimulation/Source/TA_Vehicle/Private/TARearAxleRuntime.cpp
+++ Plugins/TorqueVehicleSimulation/Source/TA_Vehicle/Private/TARearAxleRuntime.cpp
@@ -104,7 +104,8 @@ bool TARearAxleRuntime::ResolveWithTireCompliance(
     OutOutput = FTARearAxleSolveOutput{};
 
     if (DeltaTimeSeconds <= 0.0 ||
-        !TAMultiLinkSolver::ValidateConfig(Config.RightGeometry))
+        !TAMultiLinkSolver::ValidateConfig(
+            Config.RightGeometry))
     {
         return false;
     }
@@ -113,47 +114,159 @@ bool TARearAxleRuntime::ResolveWithTireCompliance(
         TAMultiLinkSolver::MirrorAcrossCenterline(
             Config.RightGeometry);
 
-    if (!TAMultiLinkSolver::ValidateConfig(LeftGeometry))
+    if (!TAMultiLinkSolver::ValidateConfig(
+            LeftGeometry))
     {
         return false;
     }
+
+    const FTARearAxleRuntimeState BaseAxleState =
+        InOutState;
+
+    const FTATireRuntimeState BaseLeftTireState =
+        InOutLeftTireState;
+
+    const FTATireRuntimeState BaseRightTireState =
+        InOutRightTireState;
+
+    double LeftAntiRollReactionN = 0.0;
+    double RightAntiRollReactionN = 0.0;
+
+    constexpr int32 CouplingIterations = 6;
+    constexpr double ReactionToleranceN = 2.0;
+
+    for (int32 Iteration = 0;
+         Iteration < CouplingIterations;
+         ++Iteration)
+    {
+        FTARearAxleRuntimeState ProbeState =
+            BaseAxleState;
+
+        FTATireRuntimeState ProbeLeftTire =
+            BaseLeftTireState;
+
+        FTATireRuntimeState ProbeRightTire =
+            BaseRightTireState;
+
+        FTAResolvedMultiLinkContact ProbeLeftContact;
+        FTAResolvedMultiLinkContact ProbeRightContact;
+
+        if (!TAMultiLinkContactResolver::ResolveCompliantRoadContact(
+                Chassis,
+                LeftGeometry,
+                Config.LeftSuspension,
+                LeftTireConfig,
+                LeftAntiRollReactionN,
+                Input.LeftDamage,
+                Input.LeftRoad,
+                DeltaTimeSeconds,
+                ProbeState.LeftGeometry,
+                ProbeState.LeftSuspension,
+                ProbeLeftTire,
+                ProbeLeftContact) ||
+            !TAMultiLinkContactResolver::ResolveCompliantRoadContact(
+                Chassis,
+                Config.RightGeometry,
+                Config.RightSuspension,
+                RightTireConfig,
+                RightAntiRollReactionN,
+                Input.RightDamage,
+                Input.RightRoad,
+                DeltaTimeSeconds,
+                ProbeState.RightGeometry,
+                ProbeState.RightSuspension,
+                ProbeRightTire,
+                ProbeRightContact))
+        {
+            return false;
+        }
+
+        const FTAAntiRollBarOutput AntiRoll =
+            TASuspensionRuntime::CalculateAntiRollBar(
+                Config.AntiRollBar,
+                ProbeLeftContact.TravelM,
+                ProbeRightContact.TravelM);
+
+        const double MaxReactionDeltaN =
+            FMath::Max(
+                FMath::Abs(
+                    AntiRoll.LeftLoadAdjustmentN
+                    - LeftAntiRollReactionN),
+                FMath::Abs(
+                    AntiRoll.RightLoadAdjustmentN
+                    - RightAntiRollReactionN));
+
+        LeftAntiRollReactionN =
+            FMath::Lerp(
+                LeftAntiRollReactionN,
+                AntiRoll.LeftLoadAdjustmentN,
+                0.75);
+
+        RightAntiRollReactionN =
+            FMath::Lerp(
+                RightAntiRollReactionN,
+                AntiRoll.RightLoadAdjustmentN,
+                0.75);
+
+        if (MaxReactionDeltaN <= ReactionToleranceN)
+        {
+            LeftAntiRollReactionN =
+                AntiRoll.LeftLoadAdjustmentN;
+
+            RightAntiRollReactionN =
+                AntiRoll.RightLoadAdjustmentN;
+
+            break;
+        }
+    }
+
+    FTARearAxleRuntimeState FinalState =
+        BaseAxleState;
+
+    FTATireRuntimeState FinalLeftTire =
+        BaseLeftTireState;
+
+    FTATireRuntimeState FinalRightTire =
+        BaseRightTireState;
 
     if (!TAMultiLinkContactResolver::ResolveCompliantRoadContact(
             Chassis,
             LeftGeometry,
             Config.LeftSuspension,
             LeftTireConfig,
+            LeftAntiRollReactionN,
             Input.LeftDamage,
             Input.LeftRoad,
             DeltaTimeSeconds,
-            InOutState.LeftGeometry,
-            InOutState.LeftSuspension,
-            InOutLeftTireState,
-            OutOutput.LeftContact))
-    {
-        return false;
-    }
-
-    if (!TAMultiLinkContactResolver::ResolveCompliantRoadContact(
+            FinalState.LeftGeometry,
+            FinalState.LeftSuspension,
+            FinalLeftTire,
+            OutOutput.LeftContact) ||
+        !TAMultiLinkContactResolver::ResolveCompliantRoadContact(
             Chassis,
             Config.RightGeometry,
             Config.RightSuspension,
             RightTireConfig,
+            RightAntiRollReactionN,
             Input.RightDamage,
             Input.RightRoad,
             DeltaTimeSeconds,
-            InOutState.RightGeometry,
-            InOutState.RightSuspension,
-            InOutRightTireState,
+            FinalState.RightGeometry,
+            FinalState.RightSuspension,
+            FinalRightTire,
             OutOutput.RightContact))
     {
         return false;
     }
 
-    TAMultiLinkContactResolver::ApplyAntiRollBarToPair(
-        Config.AntiRollBar,
-        OutOutput.LeftContact,
-        OutOutput.RightContact);
+    InOutState =
+        FinalState;
+
+    InOutLeftTireState =
+        FinalLeftTire;
+
+    InOutRightTireState =
+        FinalRightTire;
 
     OutOutput.LeftVehicleContact =
         TAMultiLinkContactResolver::BuildVehicleWheelContactInput(
